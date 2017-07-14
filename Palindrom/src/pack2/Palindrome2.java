405b63d4890a3a47cd8104a41175df5c6ee5ae69
Brahima Traore <my123gate@gmail.com>
2017-07-14 12:04:10 -0400

    Add empty and  null condition

--- Palindrom/src/pack2/Palindrome2.java
+++ Palindrom/src/pack2/Palindrome2.java
@@ -4,11 +4,15 @@ public class Palindrome2 {
 	
 	
  static boolean isPalindrome(String word){
+	 if(word.length()==0 || word==null){
+		 return false;
+	 }
 	 return word.equals(new StringBuilder(word).reverse().toString());
  }
  
  public static void main(String ...args){
 	 System.out.println(isPalindrome("ama"));//true
 	 System.out.println(isPalindrome("mama"));//false
+	 System.out.println(isPalindrome(""));//false
  }
 }
